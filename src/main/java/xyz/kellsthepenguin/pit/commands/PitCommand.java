3265dc131336f1cc17d81acea7d230302d7ee53a
kells <jwkim101201@gmail.com>
2022-02-24 15:07:58 +0900

    feat: add pit teleport

--- src/main/java/xyz/kellsthepenguin/pit/commands/PitCommand.java
+++ src/main/java/xyz/kellsthepenguin/pit/commands/PitCommand.java
@@ -1,6 +1,8 @@
 package xyz.kellsthepenguin.pit.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,6 +10,7 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static xyz.kellsthepenguin.pit.Pit.configuration;
@@ -15,6 +18,7 @@ import static xyz.kellsthepenguin.pit.Pit.playingStatuses;
 
 public class PitCommand implements CommandExecutor {
     @Override
+    @SuppressWarnings("unchecked")
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player player) {
             if (args.length != 0 && player.getWorld().getName().equals(configuration.getString("world"))) {
@@ -27,6 +31,17 @@ public class PitCommand implements CommandExecutor {
                     assert false;
                     player.getInventory().setArmorContents(((List<ItemStack>) kitSection.get("armor")).toArray(ItemStack[]::new));
                     player.getInventory().setStorageContents(((List<ItemStack>) kitSection.get("storage")).toArray(ItemStack[]::new));
+                    if (configuration.getBoolean("pit-tp")) {
+                        double[] coordinate = Arrays.stream(configuration.getString("pit-tp-location").split(" ")).mapToDouble(Double::parseDouble).toArray();
+                        Location pitSpawn = new Location(
+                                Bukkit.getWorld(configuration.getString("world")),
+                                coordinate[0],
+                                coordinate[1],
+                                coordinate[2]
+                        );
+
+                        player.teleport(pitSpawn);
+                    }
                     playingStatuses.put(uuid, true);
                 } else {
                     sender.sendMessage(ChatColor.RED + "That kit is not exist!");
